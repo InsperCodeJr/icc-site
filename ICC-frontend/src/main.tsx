3f6fe838ba423dd2c1ea1d6e50f3c430e2b5d391
nome <herbertnatanaeldecastroveras@gmail.com>
2026-05-04 00:55:40 -0300

    Adição da rota Parceiros

--- ICC-frontend/src/main.tsx
+++ ICC-frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Parceiros from './pages/Parceiros'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+  },
+  {
+    path: "/parceiros",
+    element: <Parceiros />,
   },
 ])
 
